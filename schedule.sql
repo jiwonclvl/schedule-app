7ca5129d1fc23badad927b8a8505fb13c0aabec7
abc <clvlajrrhtlvek@gmail.com>
2025-02-10 15:56:38 +0900

    Fix: "테이블 및 컬럼명 변경"
    기존의 user에서 member로 테이블명과 컬럼명을 변경해 주었다.

--- schedule.sql
+++ schedule.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `users` (
+CREATE TABLE `members` (
                          `id`	BIGINT	NOT NULL,
-                         `username`	VARCHAR (10)	NOT NULL,
+                         `membername`	VARCHAR (10)	NOT NULL,
                          `email`	VARCHAR (50)	NOT NULL UNIQUE,
                          `password`	VARCHAR (30)	NOT NULL,
                          `created_date`	DATETIME	NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `users` (
 
 CREATE TABLE `schedules` (
                              `id`	BIGINT	NOT NULL,
-                             `user_id`	BIGINT	NOT NULL,
+                             `member_id`	BIGINT	NOT NULL,
                              `title`	VARCHAR (15)	NOT NULL,
                              `contents`	VARCHAR (50)	NULL,
                              `created_date`	DATETIME	NOT NULL,
@@ -18,19 +18,19 @@ CREATE TABLE `schedules` (
 
 ALTER TABLE `schedules`
     ADD CONSTRAINT `PK_SCHEDULES`
-        PRIMARY KEY (`id`, `user_id`);
+        PRIMARY KEY (`id`, `member_id`);
 
-ALTER TABLE `users`
+ALTER TABLE `members`
     ADD CONSTRAINT `PK_USERS`
         PRIMARY KEY (`id`);
 
 ALTER TABLE `schedules`
     ADD CONSTRAINT `FK_users_TO_schedules_1`
-        FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+        FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE CASCADE;
 
 CREATE TABLE `comments` (
                          `id`	BIGINT	NOT NULL,
-                         `user_id`	BIGINT NOT NULL,
+                         `member_id`	BIGINT NOT NULL,
                          `schedule_id`	BIGINT	NOT NULL,
                          `comment` VARCHAR (50),
                          `created_date`	DATETIME	NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `comments` (
 
 ALTER TABLE `comments`
     ADD CONSTRAINT `FK_users_TO_comments`
-        FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+        FOREIGN KEY (`member_id`) REFERENCES `members` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `comments`
     ADD CONSTRAINT `FK_schedules_TO_comments_2`
