68ae355437367dc2c64b48cc5985192e3fe14686
abc <clvlajrrhtlvek@gmail.com>
2025-02-12 20:13:31 +0900

    Refactor: "세션에 저장할 로그인 상수 추가"
    로그인 상수로 세션에 넣을 값을 정의하였고 id가 아닌 유저 객체를 그대로
    담을 수 있도록 하였다.

--- src/main/java/com/example/scheduleapp/member/controller/LoginController.java
+++ src/main/java/com/example/scheduleapp/member/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.scheduleapp.member.controller;
 
+import com.example.scheduleapp.global.filter.SessionConst;
 import com.example.scheduleapp.member.dto.request.LoginRequestDto;
+import com.example.scheduleapp.member.entity.Member;
 import com.example.scheduleapp.member.service.MemberServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -29,10 +31,10 @@ public class LoginController {
             HttpServletRequest request
     ) {
         log.info("로그인 API 호출");
-        Long id = memberService.findUserByEmailAndPassword(dto.getEmail(), dto.getPassword());
+        Member loginMember = memberService.findUserByEmailAndPassword(dto.getEmail(), dto.getPassword());
 
         /*세션에 값 저장*/
-        request.getSession().setAttribute("id",id);
+        request.getSession().setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
