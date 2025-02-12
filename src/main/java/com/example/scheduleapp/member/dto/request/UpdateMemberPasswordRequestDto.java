8e2191146b056cdf4310265b3a44f5fc83da1b7b
abc <clvlajrrhtlvek@gmail.com>
2025-02-12 14:46:13 +0900

    Refactor: "유저 정보 수정 검증 처리 변경"
    비밀번호 수정 시, 사용자는 자신의 기존 비밀번호를 알고 있어야 하므로
    기존 비밀번호에는 패턴 검사를 적용할 필요가 없다고 판단하여 제거했다.

--- src/main/java/com/example/scheduleapp/member/dto/request/UpdateMemberPasswordRequestDto.java
+++ src/main/java/com/example/scheduleapp/member/dto/request/UpdateMemberPasswordRequestDto.java
@@ -7,10 +7,6 @@ import lombok.Getter;
 @Getter
 public class UpdateMemberPasswordRequestDto {
 
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
-            message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#)가 포함되어야 합니다."
-    )
     @NotBlank (message = "비밀번호를 입력해주세요")
     private String oldPassword;
 
